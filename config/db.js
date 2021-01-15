bd39a4f7c0f5dec7b1d494a3c81c61300e96f2eb
osm <remove158@gmail.com>
2021-01-15 18:47:09 +0700

    fix(mongo) : fix mongo uri

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ let mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const DBPASS = process.env.DBPASS;
-const mongo_uri = `mongodb+srv://osm:${DBPASS}@cbswebsite.o6eng.mongodb.net/test?retryWrites=true&w=majority`
+
+const mongo_uri = process.env.mongo_uri;
 mongoose.Promise = global.Promise;
 mongoose.connect(mongo_uri, { useNewUrlParser: true   , useUnifiedTopology: true }).then(
   () => {
